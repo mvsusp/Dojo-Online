55978780be46181e9e6f392bafc9eacd948dd7b3
igor dos santos montagner <igor.montagner@barney.cec.ime.usp.br>
2010-04-16 15:52:33 -0300

    missing file

--- app/metal/chat.rb
+++ app/metal/chat.rb
@@ -3,8 +3,29 @@ require(File.dirname(__FILE__) + "/../../config/environment") unless defined?(Ra
 
 class Chat
   def self.call(env)
-    if env["PATH_INFO"] =~ /^\/chat/
-      [200, {"Content-Type" => "text/html"}, ["Hello, World!"]]
+    path = env["PATH_INFO"]
+    if path =~ /^\/chat\/?/
+      session = Rack::Request.new env
+      room = session["room"].to_i
+      if env["REQUEST_METHOD"] == "GET"
+        chats = ChatMessage.find :all
+        json = "["
+        for chat in chats
+          json = json + "{user_name: \"" + chat[:poster] + "\", message: \"" + 
+            chat[:message] + "\"},"
+        end
+        json = json[0..-1] + "]"
+        puts json
+        [200, {"Content-Type" => "text/html"}, [""]]
+      else
+        generate new ChatMessage
+        new_chat = ChatMessage.new
+        new_chat[:message] = session["message"]
+        new_chat[:poster] = session.cookies["user"]
+        #new_chat[:room] = session["room"] #  rooms do not exist yet
+        new_chat.save
+        [200, {"Content-Type" => "text/html"}, [""]]
+      end
     else
       [404, {"Content-Type" => "text/html"}, ["Not Found"]]
     end
