39c6dc860597f76abb0461ecd5b1fc4f8b5a570b
Gregory De Bonis <gregorydebonis@gmail.com>
2010-04-26 11:32:44 -0300

    logout bug fixed, for real

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100319172108) do
+ActiveRecord::Schema.define(:version => 20100416175147) do
+
+  create_table "chat_messages", :force => true do |t|
+    t.string   "message"
+    t.string   "poster"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
