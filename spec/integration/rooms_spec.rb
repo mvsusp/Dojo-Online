ca7d6f66a7cd8fc962c0057343d482b85cf3c7b0
Gregory De Bonis <debonis@linux.ime.usp.br>
2010-05-21 16:20:52 -0300

    Dois testes de integração de salas adicionados. Um falha. Erro no teste?

--- spec/integration/rooms_spec.rb
+++ spec/integration/rooms_spec.rb
@@ -3,23 +3,48 @@ require 'capybara/rails'
 
 describe "Rooms" do
 
-  #after(:each) do
-  #  visit('/login/logout')
-  #end
-  #it 'should create a room with a valid name' do
-  #  visit('/')
-  #  fill_in('user[name]', :with => 'Lucas')
-  #  click_button('Login')
-  #  page.should have_content('Lucas')
-  #  visit('/rooms')
-  #  click_button('New room')
-  #  fill_in('room[name]', :with => 'Test')
-  #  fill_in('room[description]', :with => 'something')
-  #  fill_in('room[language_ids][]', :with => '[1]')
-  #  click_button('Create')
-  #  page.should have_content('Test')
-  #end
+  before(:all) do
+    Room.destroy_all
+    User.destroy_all
+    Language.destroy_all
+    @l = Language.create!(:name => 'Klingon')
 
+    visit('/')
+    fill_in('user[name]', :with => 'Batman')
+    click_button('Login')
+  end
+  
+  after(:each) do
+    Room.destroy_all
+  end
+  
+  it 'should create rooms and show them in the room list' do
+    visit('/rooms/new')
+    fill_in('room[name]', :with => 'Bat-cave')
+    fill_in('room[description]', :with => 'Saint description, Batman!')
+    check('room[language_ids][]')
+    click_button('Create')
+    page.should have_content('Welcome to the room Bat-cave, Batman')
+    page.should have_content('The owner name is Batman')
+    page.should have_content('Saint description, Batman!')
+    visit('/rooms')
+    page.should have_content('Bat-cave')
+    page.should have_content('Saint description, Batman!')
+    room = Room.find :first, :conditions => {:name => 'Bat-cave'}
+  end
 
+  it 'should refresh automatically every 10 seconds' do
+    visit('/rooms')
+    @user = User.find(:first, :conditions => {:name => 'Batman'})
+    page.should_not have_content('Hall of Justice')
+    room = Room.create! :languages => [@l], 
+                        :description => 'Room of the Super-Friends',
+                        :user => @user,
+                        :name => 'Hall of Justice',
+                        :initiated => true
+    sleep 12
+    visit('/rooms')
+    page.should have_content('Hall of Justice')
+  end                                                           
 
 end
