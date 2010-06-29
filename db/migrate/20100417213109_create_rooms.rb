4e0f39750144b185e38bb9fdf3e1daecde7556cb
Tales Pinheiro de Andrade <tales@talesp.inf.br>
2010-06-29 19:30:14 -0300

    Removido migration desnecessaria. Corrigido tipo do campo de codigo e teste para text, e nao string

--- db/migrate/20100417213109_create_rooms.rb
+++ db/migrate/20100417213109_create_rooms.rb
@@ -1,14 +1,14 @@
 class CreateRooms < ActiveRecord::Migration
   def self.up
     create_table :rooms do |t|
-      t.string :name
-      t.string :description
+      t.text :name
+      t.text :description
       t.integer :user_id     # this user is the OWNER of this room
       t.boolean :initiated   # true if this room is initiated, false otherwise
 
-      t.string :source_code
-      t.string :test_code
-      t.string :code_result
+      t.text :source_code
+      t.text :test_code
+      t.text :code_result
 
       t.timestamps
     end
